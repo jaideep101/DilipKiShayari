05a5ad95e4e936e3bdbcc51d206624ca92a597e5
JaideepSingh <jaideep.chauhan@niit-tech.com>
2018-12-23 22:13:53 +0530

    UI Changes

--- app/src/main/java/com/manorama/dilipkishayari/activities/DashboardActivity.java
+++ app/src/main/java/com/manorama/dilipkishayari/activities/DashboardActivity.java
@@ -7,8 +7,11 @@ import android.support.v7.widget.CardView;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.manorama.dilipkishayari.R;
+import com.squareup.picasso.Picasso;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -21,6 +24,15 @@ public class DashboardActivity extends AppCompatActivity {
     private CardView cardViewSeven;
     private CardView cardViewEight;
 
+    private ImageView dashboareImageviewOne;
+    private ImageView dashboareImageviewTwo;
+    private ImageView dashboareImageviewThree;
+    private ImageView dashboareImageviewFour;
+    private ImageView dashboareImageviewFive;
+    private ImageView dashboareImageviewSix;
+    private ImageView dashboareImageviewSeven;
+    private ImageView dashboareImageviewEight;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +44,22 @@ public class DashboardActivity extends AppCompatActivity {
     }
 
     private void initDashboard(){
+        dashboareImageviewOne = findViewById(R.id.dashboard_image_one);
+        setPicassoImage(dashboareImageviewOne, R.drawable.dashboard_love);
+        dashboareImageviewTwo = findViewById(R.id.dashboard_image_two);
+        setPicassoImage(dashboareImageviewTwo, R.drawable.dashboard_sad);
+        dashboareImageviewThree = findViewById(R.id.dashboard_image_three);
+        setPicassoImage(dashboareImageviewThree, R.drawable.dashboard_missing);
+        dashboareImageviewFour = findViewById(R.id.dashboard_image_four);
+        setPicassoImage(dashboareImageviewFour, R.drawable.dashboard_friends);
+        dashboareImageviewFive = findViewById(R.id.dashboard_image_five);
+        setPicassoImage(dashboareImageviewFive, R.drawable.dashboard_funny);
+        dashboareImageviewSix = findViewById(R.id.dashboard_image_six);
+        setPicassoImage(dashboareImageviewSix, R.drawable.dashboard_inspire);
+        dashboareImageviewSeven = findViewById(R.id.dashboard_image_seven);
+        setPicassoImage(dashboareImageviewSeven, R.drawable.dashboard_morning);
+        dashboareImageviewEight = findViewById(R.id.dashboard_image_eight);
+        setPicassoImage(dashboareImageviewEight, R.drawable.dashboard_wishes);
         cardViewOne = (CardView)findViewById(R.id.card_home_1);
         cardViewOne.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -88,6 +116,10 @@ public class DashboardActivity extends AppCompatActivity {
                 startHomePageActivity();
             }
         });
+    }
+
+    private void setPicassoImage(ImageView imageView, int imageResId){
+        Glide.with(DashboardActivity.this).load(imageResId).into(imageView);
     }
 
     private void startHomePageActivity(){
